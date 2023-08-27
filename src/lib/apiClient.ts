16be32b94118eec342fc143ee25365adb3b9deaf
mitanisiwi <131637872+mitanisiwi@users.noreply.github.com>
2023-08-27 15:49:12 +0900

    base url

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -2,10 +2,11 @@ import axios from  "axios";
 
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  //baseURL: "http://localhost:8000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
   headers: {
     "Content-type": "application/json",
-    //"Authorizetion": `Bearer ${token}` ,
+    
   },
 });
 
